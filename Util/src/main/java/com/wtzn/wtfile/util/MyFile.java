15fea010b7aecf99aac99ce4faa594b4bc6b03bb
vdeconinck <vincent@deconinck.info>
2021-08-02 14:09:59 +0200

    Migrated to AndroidX

--- Util/src/main/java/com/wtzn/wtfile/util/MyFile.java
+++ Util/src/main/java/com/wtzn/wtfile/util/MyFile.java
@@ -4,10 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.text.TextUtils;
-
-import com.wtzn.wtfile.util.MyMimeMap;
 
 import java.io.File;
 import java.io.FileNotFoundException;
